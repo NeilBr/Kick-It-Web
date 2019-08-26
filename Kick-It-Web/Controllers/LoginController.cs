a9a1c5df687d6a1a6f4904701bd3a0244bc1e309
Neil Brand <neilbrand1@gmail.com>
2019-08-26 22:18:37 +0200

    YEEET

---
+++ Kick-It-Web/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using Kick_It_Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Kick_It_Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(Adventurer user)
+        {
+            UserENT userent = new UserENT();
+
+            user = userent.ValidateUser(user.email, user.password);
+
+
+            return RedirectToAction("Index", "Home");
+          
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Kick-It-Web/Models/adventurer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kick_It_Web.Models
+{
+    public class Adventurer
+    {   
+       public String email, password;
+
+        public int id;
+
+        public Adventurer() {
+        }
+
+        public Adventurer(string email, string password)
+        {
+            this.email = email ;
+            this.password = password ;
+        }
+    }
+}
